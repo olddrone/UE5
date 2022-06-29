a3866f0e7d004b938dfe1715e0e438af490901df
박한길 <45356392+olddrone@users.noreply.github.com>
2022-06-30 01:19:09 +0900

    초기 커밋 메시지입니다.

---
+++ Source/Project/Character/BlasterCharacter.h
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BlasterCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+class UWidgetComponent;
+class AWeapon;
+class UCombatComponent;
+class UCombatComponent;
+
+UCLASS()
+class PROJECT_API ABlasterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABlasterCharacter();
+
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void PostInitializeComponents() override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+	void Turn(float Value);
+	void LookUp(float Value);
+
+	void EquipButtonPressed();
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+	UCameraComponent* FollowCamera;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	UWidgetComponent* OverheadWidget;
+
+	UPROPERTY(ReplicatedUsing = OnRep_OverlappingWeapon)
+	AWeapon* OverlappingWeapon;
+
+	UFUNCTION()
+	void OnRep_OverlappingWeapon(AWeapon* LastWeapon);
+
+	UPROPERTY(VisibleAnywhere)
+	UCombatComponent* Combat;
+
+	UFUNCTION(Server, Reliable)
+	void ServerEquipButtonPressed();
+
+public:
+	void SetOverlappingWeapon(AWeapon* Weapon);
+};
